fe097b562c6b0794f9f076b6851bfa92d536485d
Simoxii48 <mohahejjam@gmail.com>
2022-09-16 23:25:54 +0100

    up

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -2,6 +2,9 @@ import { useAddress, useMetamask, useNFTDrop } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
+import React, { useState } from 'react';
+import { Button, Flex, Input } from "@chakra-ui/react";
+
 
 const Mint: NextPage = () => {
   const router = useRouter();
@@ -16,11 +19,14 @@ const Mint: NextPage = () => {
     "0x72AA8E6804B58D21140aA550A11316167AF0f8a5"
   );
 
+  // handle decrement and increment Button + -
+  const [mintAmount, setMintAmount] = useState(1);
+
   async function claimNft() {
     try {
       const tx = await nftDropContract?.claim(1);
       console.log(tx);
-      alert("NFT Claimed!");
+      //alert("NFT Claimed!");
       router.push(`/stake`);
     } catch (error) {
       console.error(error);
@@ -28,6 +34,16 @@ const Mint: NextPage = () => {
     }
   }
 
+  const handleDecrement = () => {
+   if(mintAmount <= 1)return;
+   setMintAmount(mintAmount - 1);
+  }
+
+  const handleIncrement = () => {
+    if(mintAmount >= 10)return;
+    setMintAmount(mintAmount + 1);
+   }
+ 
   return (
     <div className={styles.container}>
       <h1 className={styles.h1}>Mint An NFT!</h1>
@@ -53,8 +69,51 @@ const Mint: NextPage = () => {
           Claim An NFT
         </button>
       )}
+
+      <Flex align="center" justify="center">
+           <Button
+           backgroundColor="#D6517D"
+           borderRadius="5px"
+           boxShadow="0px 2px 2px 1px #0F0F0F"
+           color="white"
+           cursor="pointer"
+           fontFamily="inherit"
+           padding="15px"
+           marginTop="10px"
+           onClick={handleDecrement}
+           >
+            -
+           </Button>
+
+           <Input 
+           readOnly
+           fontFamily="inherit"
+           width="100px"
+           height="40px"
+           textAlign="center"
+           paddingLeft="19px"
+           marginTop="10px"
+           type="number" 
+           value={mintAmount} 
+           />
+
+           <Button 
+           backgroundColor="#D6517D"
+           borderRadius="5px"
+           boxShadow="0px 2px 2px 1px #0F0F0F"
+           color="white"
+           cursor="pointer"
+           fontFamily="inherit"
+           padding="15px"
+           marginTop="10px"
+           onClick={handleIncrement}
+           >
+           +
+           </Button>
+      </Flex>     
     </div>
   );
 };
 
 export default Mint;
+
